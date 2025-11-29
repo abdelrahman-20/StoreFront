004b2dd1e5b8d8ec999edd57c65b18be40f65d1d
abdelrahman-20 <abdo.alamear25@gmail.com>
2025-11-29 17:00:06 +0200

    Register Models: (Customer) in The Admin Panel

--- store/admin.py
+++ store/admin.py
@@ -1,8 +1,20 @@
 # Registering Models
 # ------------------
-
 from django.contrib import admin
-from .models import Collection, Product
+from . import models
 
-admin.site.register(Collection)
-admin.site.register(Product)
+admin.site.register(models.Collection)
+
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ["title", "price"]
+    list_editable = ["price"]
+    list_per_page = 50
+
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ["first_name", "last_name", "membership"]
+    list_editable = ["membership"]
+    list_per_page = 50
